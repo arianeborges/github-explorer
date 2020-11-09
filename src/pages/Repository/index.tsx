c5cb55d4b37750f106c22e3502d1a28ca9ebaf8f
Ariane Borges <ariianeboorges@gmail.com>
2020-11-09 22:23:09 +0000

    Estilização do Repositório

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,7 +1,66 @@
 import React from 'react';
+import { useRouteMatch, Link } from 'react-router-dom';
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
+import api from '../../services/api';
+
+import logoImg from '../../assets/logo-github-explorer.svg';
+
+import { Header, RepositoryInfo, Issues } from './styles';
+
+interface RepositoryParams {
+  repository: string;
+}
 
 const Repository: React.FC = () => {
-  return <h1>Repository</h1>;
+  const { params } = useRouteMatch<RepositoryParams>();
+  return (
+    <>
+      <Header>
+        <img src={logoImg} alt="GithubExplorer" />
+        <Link to="/">
+          <FiChevronLeft size={16} />
+          Voltar
+        </Link>
+      </Header>
+
+      <RepositoryInfo>
+        <header>
+          <img
+            src="https://avatars2.githubusercontent.com/u/39027807?s=460&u=bf1e68cd6b60ecf06c2db2d03976cecda0b61ad7&v=4"
+            alt="img"
+          />
+          <div>
+            <strong>lala</strong>
+            <p>kaka</p>
+          </div>
+        </header>
+        <ul>
+          <li>
+            <strong>1808</strong>
+            <span>Stars</span>
+          </li>
+          <li>
+            <strong>48</strong>
+            <span>Forks</span>
+          </li>
+          <li>
+            <strong>67</strong>
+            <span>Issues Abertas</span>
+          </li>
+        </ul>
+      </RepositoryInfo>
+
+      <Issues>
+        <Link to="/">
+          <div>
+            <strong>fsf</strong>
+            <p>fasfsa</p>
+          </div>
+          <FiChevronRight size={20} />
+        </Link>
+      </Issues>
+    </>
+  );
 };
 
 export default Repository;
