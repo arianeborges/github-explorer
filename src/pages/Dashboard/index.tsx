2cbdc754c6952889900052339f7bd36fb8e8f4b0
Ariane Borges <ariianeboorges@gmail.com>
2020-11-09 16:48:13 +0000

    Configurações de Rotas

---
+++ src/pages/Dashboard/index.tsx
@@ -0,0 +1,9 @@
+/* eslint-disable import/no-unresolved */
+/* eslint-disable no-use-before-define */
+import React from 'react';
+
+const Dashboard: React.FC = () => {
+  return <h1>Dashboard</h1>;
+};
+
+export default Dashboard;
